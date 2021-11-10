20eae0cb595196d80a48db39fb232b055f069303
CESAR AUGUSTO MEJIA BEDOYA <cesarmejia86480@correo.itm.edu.co>
2021-11-10 10:29:55 -0500

    se agrega la clase prestamo y estado de material

---
+++ WebSib/Clases/clsEstadoMaterial.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// Referenciar y Usar
+using System.Web.UI.WebControls;
+using webSib.Clases;
+
+namespace WebSib.Clases
+{
+    public class clsEstadoMaterial
+    {
+        #region "Atributos y propiedades"
+        private string strApp;
+        public string Error { private set; get; }
+        private string strSQL;
+        #endregion
+        #region "Constructor"
+        public clsEstadoMaterial(string Aplicacion)
+        {
+            strApp = Aplicacion;
+            strSQL = string.Empty;
+            Error = string.Empty;
+        }
+        #endregion
+
+        #region "Metodos publicos"
+        public bool llenarCombo(DropDownList Combo)
+        {
+            try
+            {
+                if (Combo == null)
+                {
+                    Error = "Error";
+                    return false;
+                }
+                strSQL = "EXEC sp_consultar_Mat_Estado;";
+                clsGenerales objGles = new clsGenerales();
+                if (!objGles.llenarCombo(strApp, Combo, strSQL, "Clave", "Dato"))
+                {
+                    Error = objGles.Error;
+                    objGles = null;
+                    return false;
+                }
+                objGles = null;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Error = ex.Message;
+                return false;
+            }
+        }
+        #endregion
+    }
+}
