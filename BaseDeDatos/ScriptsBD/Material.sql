454b33b27dcbbf1973524e1450b90132da7bd777
StivenManco26 <manco.5@hotmail.com>
2021-11-09 22:08:03 -0500

    Se inicia con el desarrollo del módulo de material

---
+++ BaseDeDatos/ScriptsBD/Material.sql
@@ -0,0 +1,200 @@
+--MÓDULO MATERIAL------------------------------------------------------
+CREATE PROCEDURE sp_consultar_Mat_general
+AS
+BEGIN
+	SELECT *
+	FROM tblMaterial
+	ORDER BY codigo ASC 
+END
+GO
+
+
+CREATE PROCEDURE sp_consultar_Mat_puntual
+@codigo VARCHAR(30)
+AS
+BEGIN
+	SELECT *
+	FROM tblMaterial
+	WHERE codigo=@codigo
+	ORDER BY codigo ASC 
+END
+GO
+
+
+CREATE PROCEDURE sp_consultar_Mat_Estado
+AS
+BEGIN
+	SELECT id as Clave, estado as Dato
+	FROM tblMaterialEstado
+	ORDER BY id ASC 
+END
+GO
+
+
+CREATE PROCEDURE sp_consultar_Mat_Productor
+AS
+BEGIN
+	SELECT id as Clave, productor as Dato
+	FROM tblMaterialProductor
+	ORDER BY id ASC 
+END
+GO
+
+
+CREATE PROCEDURE sp_consultar_Mat_Autor 
+AS
+BEGIN
+	SELECT id as Clave, autor as Dato
+	FROM tblMaterialAutor
+	ORDER BY id ASC 
+END
+GO
+
+
+CREATE PROCEDURE sp_ingresar_Mat_autor
+@nombre VARCHAR(200)
+AS
+BEGIN
+
+IF NOT EXISTS (SELECT autor FROM tblMaterialAutor WHERE autor=@nombre)
+BEGIN
+
+	BEGIN TRANSACTION tx
+
+		INSERT INTO tblMaterialAutor (autor)
+		VALUES (UPPER(@nombre));
+
+		IF ( @@ERROR > 0 )
+		BEGIN
+			ROLLBACK TRANSACTION tx
+			SELECT 0 AS Rpta
+			RETURN
+		END
+
+	COMMIT TRANSACTION tx
+	SELECT 1 AS Rpta
+	RETURN
+
+END
+ELSE 
+	SELECT 0 AS Rpta
+	RETURN
+END
+
+--EXEC sp_ingresar_autor 'Gabriel garcia marquez'
+GO
+
+
+CREATE PROCEDURE sp_ingresar_Mat_productor
+@nombre VARCHAR(200)
+AS
+BEGIN
+
+IF NOT EXISTS (SELECT productor FROM tblMaterialProductor WHERE productor=@nombre)
+BEGIN
+
+	BEGIN TRANSACTION tx
+
+		INSERT INTO tblMaterialProductor (productor)
+		VALUES (UPPER(@nombre));
+
+		IF ( @@ERROR > 0 )
+		BEGIN
+			ROLLBACK TRANSACTION tx
+			SELECT 0 AS Rpta
+			RETURN
+		END
+
+	COMMIT TRANSACTION tx
+	SELECT 1 AS Rpta
+	RETURN
+
+END
+ELSE 
+	SELECT 0 AS Rpta
+	RETURN
+END
+
+--EXEC sp_ingresar_productor 'planeta'
+GO
+
+
+CREATE PROCEDURE sp_ingresar_Material
+@codigo VARCHAR(30),
+@nombre VARCHAR(200),
+@edicion VARCHAR(10),
+@cantidad INT,
+@idEstado INT,
+@idAutor INT,
+@idProductor INT
+AS
+BEGIN
+
+IF NOT EXISTS (SELECT codigo FROM tblMaterial WHERE codigo=@codigo)
+BEGIN
+
+	BEGIN TRANSACTION tx
+
+		INSERT INTO tblMaterial(codigo,nombre,edicion,cantidad,idEstado,idAutor,idProductor)
+		VALUES (UPPER(@codigo),UPPER(@nombre),UPPER(@edicion),@cantidad,@idEstado,@idAutor,@idProductor);
+
+		IF ( @@ERROR > 0 )
+		BEGIN
+			ROLLBACK TRANSACTION tx
+			SELECT 0 AS Rpta
+			RETURN
+		END
+
+	COMMIT TRANSACTION tx
+	SELECT 1 AS Rpta
+	RETURN
+
+END
+ELSE 
+	SELECT 0 AS Rpta
+	RETURN
+END
+
+--EXEC sp_ingresar_Material 'CaS0001','CIEN año DE SOLedad','2.0',2,1,1,1
+GO
+
+
+CREATE PROCEDURE sp_actualizar_Material
+@codigo VARCHAR(30),
+@nombre VARCHAR(200),
+@edicion VARCHAR(10),
+@cantidad INT,
+@idEstado INT,
+@idAutor INT,
+@idProductor INT
+AS
+BEGIN
+
+IF NOT EXISTS (SELECT codigo FROM tblMaterial WHERE codigo=@codigo)
+BEGIN
+
+	BEGIN TRANSACTION tx
+
+		UPDATE tblMaterial
+		SET nombre=UPPER(@nombre),edicion=UPPER(@edicion),cantidad=@cantidad,idEstado=@idEstado,idAutor=@idAutor,idProductor=@idProductor
+		WHERE codigo=@codigo;
+
+		IF ( @@ERROR > 0 )
+		BEGIN
+			ROLLBACK TRANSACTION tx
+			SELECT 0 AS Rpta
+			RETURN
+		END
+
+	COMMIT TRANSACTION tx
+	SELECT 1 AS Rpta
+	RETURN
+
+END
+ELSE 
+	SELECT 0 AS Rpta
+	RETURN
+END
+
+--EXEC sp_actualizar_Material 'CaS0001','CIEN añoS DE SOLedad','3.0',2,1,1,1
+GO
---
+++ WebSib/frmMaterial.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace webSib
+{
+
+
+    public partial class frmMaterial1
+    {
+
+        /// <summary>
+        /// Control lblUsu.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsu;
+
+        /// <summary>
+        /// Control txtCodigo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodigo;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control txtEdicion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEdicion;
+
+        /// <summary>
+        /// Control txtCantidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCantidad;
+
+        /// <summary>
+        /// Control ddlEstado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlEstado;
+
+        /// <summary>
+        /// Control ddlAutor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlAutor;
+
+        /// <summary>
+        /// Control ddlEditorial.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlEditorial;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+
+        /// <summary>
+        /// Control btnCancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCancelar;
+
+        /// <summary>
+        /// Control mnuOpciones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu mnuOpciones;
+
+        /// <summary>
+        /// Control lblMsj.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMsj;
+    }
+}
